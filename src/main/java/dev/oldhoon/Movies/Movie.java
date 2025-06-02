d8dd677470db78bfe6b94d09c9555422f4bc5ed2
Young <kimyoh123@gmail.com>
2025-06-01 23:47:17 -0700

    movie class

---
+++ src/main/java/dev/oldhoon/Movies/Movie.java
@@ -0,0 +1,29 @@
+package dev.oldhoon.Movies;
+
+import java.util.List;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "movies") // documents 
+@Data // takes care of getters and setters
+@AllArgsConstructor // constructor that takes all args as arguments
+@NoArgsConstructor // constructor with no arg 
+
+public class Movie {
+    @Id // unique identifier for each movie in db
+    private ObjectId id;
+    private String imdbId;
+    private String title; 
+    private String releaseDate;
+    private String trailerLinkString;    
+    private String poster;
+    private String genres;
+    private List<String> backdrops;
+    private List<String> reviewIds;
+}
