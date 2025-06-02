d34211ffbe03008035575645097f911f652ecb6f
Young <kimyoh123@gmail.com>
2025-06-01 23:41:26 -0700

    initial commit

---
+++ src/main/java/dev/oldhoon/Movies/MoviesApplication.java
@@ -0,0 +1,14 @@
+package dev.oldhoon.Movies;
+
+import org.springframework.boot.SpringApplication; //contains run method
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // annotation
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/dev/oldhoon/Movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.oldhoon.Movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
